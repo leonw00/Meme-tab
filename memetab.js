2544b56593f543678035cb82f89fedc02017f968
leo <leonardow1908@gmail.com>
2021-10-19 20:53:32 -0500

    initialize and basic structure

---
+++ memetab.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function (){
+    var randImg = Math.floor(Math.random() * 3);
+    var meme = document.getElementById("meme_img");
+    var newString = "memes/meme" + String(randImg) + ".jpeg";
+    meme.src = newString;
+}, false)
+
